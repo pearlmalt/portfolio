2f2a2af3567673b2b2bd824f560e15ea14ea4130
pearlmalt <maltpearl@gmail.com>
2021-11-16 12:39:56 -0800

    Updates

---
+++ precache-manifest.deabfcfa53958e73eddb3468b52b4d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcf39d17f68eb1a54c925fd88bc0abf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "12da5af11e09fcbe3dbe",
+    "url": "/portfolio/static/css/main.af0ac2df.chunk.css"
+  },
+  {
+    "revision": "a0451c57bffcc1ff86d5",
+    "url": "/portfolio/static/js/2.75dc6be4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.75dc6be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12da5af11e09fcbe3dbe",
+    "url": "/portfolio/static/js/main.625708c5.chunk.js"
+  },
+  {
+    "revision": "33e3eb3d60fc778ec147",
+    "url": "/portfolio/static/js/runtime-main.d2c2cec5.js"
+  }
+]);
